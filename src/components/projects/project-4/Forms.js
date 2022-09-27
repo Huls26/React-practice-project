18daa7d8522f29bdca3620c987cfe0cff01d2fa7
Jules Moreno <julesmoreno66@gmail.com>
2022-09-27 11:10:34 +0800

    project4.1 update

--- src/components/projects/project-4/Forms.js
+++ src/components/projects/project-4/Forms.js
@@ -6,7 +6,10 @@ export default function Form() {
                                     name: "",
                                     email: "",
                                 }));
+    let [list, setList] = useState(() => ([]));
 
+    let listElement = list.map(person => <List key={person.name} name={person.name} email={person.email} />)
+    // event
     function updateForm(event) {
         let name = event.currentTarget.name;
         let value = event.currentTarget.value;
@@ -19,22 +22,37 @@ export default function Form() {
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(form, "added")
+        setList(prevList => ([...prevList, form]));
     }
 
     return (
-        <form action="" className="form-addperson" onSubmit={ handleSubmit }>
-            <div className="name-form">
-                <label htmlFor="name">Name:</label>
-                <input value={ form.name } type="text" id="name" name="name" onChange={ event => updateForm(event) }/>
-            </div>
-         
-            <div className="email-form">
-                <label htmlFor="email">Email:</label>
-                <input value={ form.email } type="email" id="email" name="email" onChange={ event => updateForm(event) } />
-            </div>
+        <div className="form-container">
+              <form action="" className="form-addperson" onSubmit={ handleSubmit }>
+                <div className="name-form">
+                    <label htmlFor="name">Name:</label>
+                    <input value={ form.name } type="text" id="name" name="name" onChange={ event => updateForm(event) }/>
+                </div>
+            
+                <div className="email-form">
+                    <label htmlFor="email">Email:</label>
+                    <input value={ form.email } type="email" id="email" name="email" onChange={ event => updateForm(event) } />
+                </div>
 
-            <button className="add-person">Add Person</button>
-        </form>
+                <button className="add-person">Add Person</button>
+            </form>
+
+            <div className="list-container">
+                { listElement }
+            </div>
+        </div>
+    )
+}
+
+function List({name, email}) {
+    return (
+        <article className="info">
+            <h3 className="info-name">{ name }</h3>
+            <p className="info-email">{ email }</p>
+        </article>
     )
 }
